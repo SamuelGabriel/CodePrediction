9cb868f185bcb5b5c5008697ff6ffc896fd8ef88
Samuel Müller <samuelmueller@Samuels-MacBook-797.local>
2019-02-10 08:51:46 +0000

    fixed coda.py

--- machine-learning-for-programming-samples/coda.py
+++ machine-learning-for-programming-samples/coda.py
@@ -3,6 +3,7 @@ import sys
 
 # usage: python coda.py <name> <other args>
 
+subprocess.run(['cl', 'work', 'sgm-rnn-tutorial'])
 cmd ="python3 language_model/train.py {} trained_models/ corpus/train corpus/valid".format(' '.join(
-    "'{}'".format(e) if e[0]=='{' else e for e in sys.argv[1:]))
-subprocess.run(['cl', 'run', ':corpus', ':language_model', cmd, '--request-gpus', '1', '--request-docker-image', 'samgmuller/tf1.12base:0.3', "--name", sys.argv[0]])
+    "'{}'".format(e) if e[0]=='{' else e for e in sys.argv[2:]))
+subprocess.run(['cl', 'run', ':corpus', ':language_model', cmd, '--request-gpus', '1', '--request-docker-image', 'samgmuller/tf1.12base:0.3', "--name", sys.argv[1]])
