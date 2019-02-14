8d93343e02522a21863fb717c7f67fe3f31faeea
Samuel Müller <samuelmueller@Samuels-MacBook-797.local>
2019-02-14 11:50:39 +0000

    nicer coda helper

--- src/coda.py
+++ src/coda.py
@@ -1,9 +1,19 @@
 import subprocess
 import sys
+from argparse import ArgumentParser
 
 # usage: python coda.py <name> <other args>
 
+parser = ArgumentParser()
+parser.add_argument('--name')
+parser.add_argument('--hypers-override', dest='hypers_override', default='{}')
+parser.add_argument('--models-override', dest='models_override', default='{}')
+parser.add_argument('--max-out', dest='max_out', default=False)
+args = parser.parse_args()
+
+extra_requests = ['--request-cpus', '6', '--request-memory', '56g'] if args.max_out else []
+
 subprocess.run(['cl', 'work', 'sgm-rnn-tutorial'])
 cmd ="python3 language_model/train.py {} trained_models/ corpus/train corpus/valid".format(' '.join(
-    "'{}'".format(e) if e[0]=='{' else e for e in sys.argv[2:]))
-subprocess.run(['cl', 'run', ':corpus', ':language_model', cmd, '--request-gpus', '1', '--request-docker-image', 'samgmuller/tf1.12base:0.4', "--name", sys.argv[1]])
+    ['--hypers-override', "\'"+args.hypers_override+"\'", '--models-override', "\'"+args.models_override+"\'"]))
+subprocess.run(['cl', 'run', ':corpus', ':language_model', cmd, '--request-gpus', '1', '--request-docker-image', 'samgmuller/tf1.12base:0.4', "--name", args.name] + extra_requests)
