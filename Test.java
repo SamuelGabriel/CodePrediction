c121513571b7b6bff14fb9d80ba79500fa05fcad
Samuel Müller <samuelmueller@Samuels-MacBook-797.local>
2019-02-23 15:15:34 +0000

    small changes to examples and codalab runner

--- Test.java
+++ Test.java
@@ -1,10 +1,15 @@
 package hdfs;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
 
 
 public class Test {
-	public int a = 1;
+	public String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 	public int plus_random_and_a(int b){
-		return a + b + ThreadLocalRandom.current().nextInt();
+		ArrayList<Integer> c = new ArrayList<Integer>();
+		c.add(4);
+		b = 1;
+		int d = 10;
+		return cars[1].length() + c.get(0) + b + ThreadLocalRandom.current().nextInt();
 	}
 }
--- src/coda.py
+++ src/coda.py
@@ -8,12 +8,17 @@ parser = ArgumentParser()
 parser.add_argument('--name')
 parser.add_argument('--hypers-override', dest='hypers_override', default='{}')
 parser.add_argument('--models-override', dest='models_override', default='{}')
-parser.add_argument('--max-out', dest='max_out', default=False)
+parser.add_argument('--max-out', action='store_true')
+parser.add_argument('--typed-dataset', action='store_true')
 args = parser.parse_args()
 
-extra_requests = ['--request-cpus', '6', '--request-memory', '56g'] if args.max_out else []
+corpus_name = 'typedcorpus' if args.typed_dataset else 'corpus'
+
+extra_requests = ['--request-cpus', '6', '--request-memory', '55g'] if args.max_out else []
 
 subprocess.run(['cl', 'work', 'sgm-rnn-tutorial'])
-cmd ="python3 language_model/train.py {} trained_models/ corpus/train corpus/valid".format(' '.join(
-    ['--hypers-override', "\'"+args.hypers_override+"\'", '--models-override', "\'"+args.models_override+"\'"]))
-subprocess.run(['cl', 'run', ':corpus', ':language_model', cmd, '--request-gpus', '1', '--request-docker-image', 'samgmuller/tf1.12base:0.4', "--name", args.name] + extra_requests)
+cmd ="python3 language_model/train.py {} ".format(' '.join(
+        ['--hypers-override', "\'"+args.hypers_override+"\'", '--models-override', "\'"+args.models_override+"\'"]
+     )) \
+     + f"trained_models/ {corpus_name}/train {corpus_name}/valid"
+subprocess.run(['cl', 'run', ':' + corpus_name, ':language_model', cmd, '--request-gpus', '1', '--request-docker-image', 'samgmuller/tf1.12base:0.4', "--name", args.name] + extra_requests)
